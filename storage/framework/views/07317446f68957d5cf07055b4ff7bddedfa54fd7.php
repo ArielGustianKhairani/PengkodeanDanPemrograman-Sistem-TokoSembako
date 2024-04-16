6973ab4f93966b100851230c32ce5e630d796b11
ArielGustianKhairani <khairaiav22@gmail.com>
2024-04-16 17:38:44 +0700

    first commit

---
+++ storage/framework/views/07317446f68957d5cf07055b4ff7bddedfa54fd7.php
@@ -0,0 +1,7 @@
+<footer class="main-footer">
+    <div class="pull-right hidden-xs">
+        <b>Version</b> 1.1
+    </div>
+    <strong>Copyright &copy; <?php echo e(date('Y')); ?> <a href="/"><?php echo e($setting->nama_perusahaan); ?></a>.</strong> All rights
+    reserved.
+</footer><?php /**PATH C:\Users\rshaf_jv5mv1h\Downloads\punya khai\Point-Of-Sale-Lara-8\resources\views/layouts/footer.blade.php ENDPATH**/ ?>
